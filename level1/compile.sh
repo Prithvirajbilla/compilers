57239f73f406222e5e9878063641db9ada468aa8
prithvirajbilla <prithviraj.m.billa@gmail.com>
2014-02-06 17:42:20 +0530

    :+1: level up to 1

---
+++ level1/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+make -f Makefile.cfglp
+cd test_files
+for i in $(ls *.c)
+do
+	cd ..
+	echo $i
+	make -f Makefile.cfg FILE=$i > /dev/null
+	./cfglp -d -ast test_files/${i}s306.cfg > our.tok
+	./cfglp32 -d -ast test_files/${i}s306.cfg > sir.tok
+	diff -b -B our.tok sir.tok
+	cd test_files
+done
+cd ..
+rm sir.tok 
+rm our.tok
+exit
